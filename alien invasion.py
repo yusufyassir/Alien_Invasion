cbb2cc3f6ad029abe0c35596b8414e541cd18231
yusufyassir <yusufyassir0@gmail.com>
2023-04-02 09:04:02 +0200

    stated on games background

---
+++ alien invasion.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+
+class AlienIvasion:
+    """overall class to manage game assets and behaviour"""
+    def __init__(self):
+        """initialize the game, and create game resources"""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 620))
+        pygame.display.set_caption("Alien Invasion")
+
+        #set background color
+        self.bg_color = (230,230,230)
+    def run_game(self):
+        """start the game main loop"""
+        while True:
+            #watch for keboard and muse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #redraw screen through each pass of looop
+            self.screen.fill(self.bg_color)
+            #make most recet screen drawn
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #make game instance and run ngame
+    ai = AlienIvasion()
+    ai.run_game()
