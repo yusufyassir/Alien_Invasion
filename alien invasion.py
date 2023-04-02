b45a6f4d93f76ceea12d9ff97120dc47ca9a3b1a
yusufyassir <yusufyassir0@gmail.com>
2023-04-02 16:38:38 +0200

    added left\right movment and quits with 'q'

--- alien invasion.py
+++ alien invasion.py
@@ -18,15 +18,45 @@ class AlienIvasion:
     def run_game(self):
         """start the game main loop"""
         while True:
-            #watch for keboard and muse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            #redraw screen through each pass of looop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.bltime()
-            #make most recet screen drawn
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_sceen()
+         
+
+    def _check_events(self):
+        """responds to key presse and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            #for moving right
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events
+
+    def _check_keydown_events(self, event):
+        """responds to key presses"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """responds to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_sceen(self):
+        """update images on screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.bltime()
+        #make most recet screen drawn
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     #make game instance and run ngame
--- ship.py
+++ ship.py
@@ -6,15 +6,30 @@ class Ship:
     def __init__(self, ai_game):
         """initiliaze the ship and set satrating position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-
         #load the ship and its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-
         #start each ne ship at the bottom corer of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        #store decimal value for the ship horizontal position.
+        self.x = float(self.rect.x)
+        #movement flag
+        self.moving_right = False
+        self.moving_left = False
 
     def bltime(self):
         """"draw ship in its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """update the ships position"""
+        #update the shp x value, not rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #update rect object from self.x
+        self.rect.x = self.x
