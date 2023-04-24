9b8137a9b3b3eded4887171bc31060aaf58246de
yusufyassir <yusufyassir0@gmail.com>
2023-04-24 14:59:43 +0200

    dynamicc and static settings

--- settings.py
+++ settings.py
@@ -8,16 +8,27 @@ class Settings:
         self.screen_height = 620
         self.bg_color = (230, 230, 230)
         #ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
         #bullet settings
-        self.bullet_speed = 1.5
         self.bullet_width = 20
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
         #alien settingd
-        self.alien_speed = .5
         self.fleet_drop_speed = 6
+
+        #how quickly the game speeds up
+        self.sppedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """initialize settings that change troughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 1.5
+        self.alien_speed = .5
+
         #fleet direction of 1 is right, -1 is left
         self.fleet_direction = 1
+
+        
